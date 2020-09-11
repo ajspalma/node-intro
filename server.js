0ea1494d95a8d97975160738790db4c51bceb83a
ajspalma <palma.ashleyjs@gmail.com>
2020-09-12 00:03:50 +0800

    Intro to RESTful API and express request and response object and serving static files using express.static method

--- server.js
+++ server.js
@@ -4,37 +4,14 @@ const bodyParser = require('body-parser');
 
 const app = express();
 
-app.use((req, res, next) => {
-    console.log('Hello');
-    // run next() method to run the next request 
-    next();
-});
+// app.get('/', (req, res) => {
+//     // req.query // Query Strings
+//     // req.body // Body 
+//     // req.headers // Headers
+//     // console.log(req.params) // URL Parameters ('/:id')
+//     res.status(200).send('getting root');
+// });
 
-// Body-Parser for x-wwww-form-urlencoded
-app.use(bodyParser.urlencoded({ extended: false}));
-
-// Body-Parser for raw (JSON)
-app.use(bodyParser.json());
-
-// get, post, put, delete
-
-app.get('/', (req, res) => {
-    // Automatic Header Content-Type detection
-    // res.send("hello");
-    // res.send("<h1>hello</h1>");
-    res.send({
-        name: 'John',
-        hobby: 'Painting'
-    });
-});
-
-app.post('/profile', (req, res) => {
-    console.log(req.body); // returns undefined (needs body parser)
-    res.send('Success');
-});
-
-app.get('/profile', (req, res) => {
-    res.send('getting profile');
-});
+app.use(express.static(__dirname + '/public'));
 
 app.listen(4000);
