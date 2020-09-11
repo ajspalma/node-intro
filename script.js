1bf1f812ba9486f4122ddde6137928ea7ebe77b6
ajspalma <palma.ashleyjs@gmail.com>
2020-09-11 21:50:10 +0800

    Initial commit

---
+++ script.js
@@ -0,0 +1,12 @@
+const a = 4;
+const b = 5;
+
+// logs and evaluate/compute
+console.log(a + b);
+
+// logs the (current) Directory/path name 
+console.log(__dirname);
+
+// setTimeout(() => {
+//     console.log(a + b);
+// }, 3000);
