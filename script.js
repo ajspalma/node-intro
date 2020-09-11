3e6b366c5544954ae677ab67bdbb66b88dab6c4c
ajspalma <palma.ashleyjs@gmail.com>
2020-09-11 22:02:08 +0800

    Using Modules in Common JS way

--- script.js
+++ script.js
@@ -10,3 +10,11 @@ console.log(__dirname);
 // setTimeout(() => {
 //     console.log(a + b);
 // }, 3000);
+
+// Import module using require (common js way)
+const script2 = require('./script2.js');
+
+const num1 = script2.largeNumber;
+const num2 = 5;
+
+console.log(num1 + num2);
---
+++ script2.js
@@ -0,0 +1,6 @@
+const largeNumber = 95;
+
+// exporting module in common js way 
+module.exports = {
+    largeNumber: largeNumber
+};
